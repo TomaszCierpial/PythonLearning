878b2091bf3e2cb7f27a63c152e53e56d408efa6
Tomasz Cierpiał <tcierpial@sii.pl>
2024-03-12 20:04:24 +0100

    Download data from web page and save it to file as bytes

--- TakeDataFromWeb/takedata.py
+++ TakeDataFromWeb/takedata.py
@@ -1,5 +1,6 @@
 """Connect to webpage for data collect
 """
+import io
 import requests
 
 class HttpData:
@@ -19,9 +20,52 @@ class HttpData:
         """
         try:
             self.url = url
-            return requests.get(self.url, timeout = 10).status_code
+            return requests.get(self.url, timeout = 10)
         except requests.HTTPError as err:
+            print(err)
             return err
+    def save_results_in_file(self, filename: str, datatobesaved, savemode: str):
+        """Save collected data to file
+
+        Args:
+            filename (str): Result file name
+            datatobesaved (str): Data to be saved
+            savemode (str): Opening mode
+            The argument mode points to a string beginning with one of the following
+            sequences (Additional characters may follow these sequences.):
+
+            ``r''   Open text file for reading.  The stream is positioned at the
+                    beginning of the file.
+
+            ``r+''  Open for reading and writing.  The stream is positioned at the
+                    beginning of the file.
+
+            ``w''   Truncate file to zero length or create text file for writing.
+                    The stream is positioned at the beginning of the file.
+
+            ``w+''  Open for reading and writing.  The file is created if it does not
+                    exist, otherwise it is truncated.  The stream is positioned at
+                    the beginning of the file.
+
+            ``a''   Open for writing.  The file is created if it does not exist.  The
+                    stream is positioned at the end of the file.  Subsequent writes
+                    to the file will always end up at the then current end of file,
+                    irrespective of any intervening fseek(3) or similar.
+
+            ``a+''  Open for reading and writing.  The file is created if it does not
+                    exist.  The stream is positioned at the end of the file.  Subse-
+                    quent writes to the file will always end up at the then current
+                    end of file, irrespective of any intervening fseek(3) or similar.
+            
+            ``wb''  Truncate file to zero length or create text file for writing.
+                    The stream is positioned at the beginning of the file.
+        """
+        try:
+            with io.open(filename, savemode) as file:
+                file.write(datatobesaved)
+        except IOError as err:
+            print(err)
 
 HttpDataObject = HttpData()
-print(HttpDataObject.connect_to_web_page_and_return_data('https://justjoin.it/'))
+pagecontent = HttpDataObject.connect_to_web_page_and_return_data('https://justjoin.it/')
+HttpDataObject.save_results_in_file("Results.txt", pagecontent.content, "wb")
